f4f2862279e22ec8b111df72afcd16f1001d00f3
Bendik Aagaard Lynghaug <bendik.lynghaug@gmail.com>
2020-07-21 18:42:57 +0200

    precompile service file binary paths

--- bin/commands/systemd-setup.js
+++ bin/commands/systemd-setup.js
@@ -9,23 +9,45 @@ module.exports = class SystemdCommand extends Command {
   static description = 'Set up your systemd `.service` files for hyperspace and hyperdrive'
 
   async run () {
+    const services = ['hyperspace', 'hyperdrive']
     try {
-      if (os.platform() !== 'linux') throw 'Only supported on linux'
-      let { status: noSystemd } = spawnSync('which', ['systemctl'])
-      if (noSystemd) throw 'Systemd doesn\'t seem to be installed'
-      const hyperspaceService = fs.readFileSync(p.join(__dirname, '../../hyperspace.service'))
-      const hyperdriveService = fs.readFileSync(p.join(__dirname, '../../hyperdrive.service'))
+      if (os.platform() !== 'linux') throw new Error('Only supported on linux')
+      const { status: noSystemd } = spawnSync('which', ['systemctl'])
+      if (noSystemd) throw new Error('Systemd doesn\'t seem to be installed')
+      const { status: noHyperspace } = spawnSync('which', ['hyperspace'])
+      if (noHyperspace) throw new Error('Hyperspace is not installed')
+      const { stdout: npmPrefix } = spawnSync('npm', ['config', 'get', 'prefix'])
+      if (!npmPrefix) throw new Error('npm missing')
       const serviceDir = os.homedir() + '/.config/systemd/user'
-      spawnSync('mkdir', ['-p', serviceDir])
-      fs.writeFileSync(serviceDir + '/hyperspace.service', hyperspaceService)
-      fs.writeFileSync(serviceDir + '/hyperdrive.service', hyperdriveService)
+
+      services.map(readServiceFile)
+        .map(prefixServiceFileBinaryPath(npmPrefix.toString().trim()))
+        .forEach(writeToServicesDirectory(serviceDir))
+
       console.log(`Successfully installed service files for hypercore and hyperdrive in ${serviceDir}`)
       console.log('To start hyperdrive and hyperspace server with systemctl just run:\n')
       console.log('systemctl --user start hyperdrive\n')
       console.log('… or start it now and have it persist between reboots with:\n')
       console.log('systemctl --user enable --now hyperdrive')
     } catch (err) {
+      console.log('Failed setting up systemd service files:')
       console.error(err)
+    }
+    function readServiceFile (name) {
+      return { name, file: fs.readFileSync(p.join(__dirname, `../../${name}.service`), { encoding: 'utf8' }) }
+    }
+    function prefixServiceFileBinaryPath (prefix) {
+      return (service) => {
+        const i = service.file.indexOf('ExecStart=') + 10
+        return Object.assign(service, {
+          file: [service.file.substring(0, i), prefix, '/bin/', service.file.substring(i, service.file.length)].join('')
+        })
+
+      }
+    }
+    function writeToServicesDirectory (dir) {
+      spawnSync('mkdir', ['-p', dir])
+      return (service) => fs.writeFileSync(dir + `/${service.name}.service`, service.file)
     }
   }
 }
